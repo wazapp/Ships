5d2ee7e5cd25a5f3e63dbb2756345c5a77800b8b
Tor Hammar <mengedej@gmail.com>
2011-06-13 21:01:42 +0200

    Initial project setup

---
+++ src/com/gmail/wazappdotgithub/ships/Ships.java
@@ -0,0 +1,13 @@
+package com.gmail.wazappdotgithub.ships;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ships extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
