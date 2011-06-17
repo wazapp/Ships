c239d2965ab914a7753946d83872d8bc96567558
Tor Hammar <mengedej@gmail.com>
2011-06-17 23:01:46 +0200

    rotateship button now works

--- src/com/gmail/wazappdotgithub/ships/PreGame.java
+++ src/com/gmail/wazappdotgithub/ships/PreGame.java
@@ -34,9 +34,17 @@ public final class PreGame extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Log.d("Ships PreGame","rotatingShip");
-				board.toggleOrientation(0);
-				findViewById(R.id.pregame_BoardView).invalidate();
+				
+				/* TODO some method that will read the latest position, and return the ship id, if any
+				 * BoardView could use the latest position of the selectRectangle and return the shipid 
+				 */
+				
+				int id = ((BoardView) findViewById(R.id.pregame_BoardView)).shipIdUnderCursor();
+				Log.d("Ships PreGame","rotatingShip id " + id);
+				if ( id >= 0 ) {
+					board.toggleOrientation(id);
+					findViewById(R.id.pregame_BoardView).invalidate();
+				}
 			}
 		});
 	}	
