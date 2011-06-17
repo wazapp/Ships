304dfbaf4ce1174aca1f8681a92af0d2b8e3ebf7
Tor Hammar <mengedej@gmail.com>
2011-06-17 22:24:50 +0200

    set a condition for the ship sizes array

--- src/com/gmail/wazappdotgithub/ships/common/Constants.java
+++ src/com/gmail/wazappdotgithub/ships/common/Constants.java
@@ -5,6 +5,7 @@ package com.gmail.wazappdotgithub.ships.common;
  */
 public class Constants {
     public static final int DEFAULT_BOARD_SIZE = 10;
+    //largest ship at the last position
     public static final int[] DEFAULT_SHIPS = new int[] {2,2,2,2,3,3,3,4,4,6};
     public static final int DEFAULT_SHIPS_NUM = DEFAULT_SHIPS.length;
 }
