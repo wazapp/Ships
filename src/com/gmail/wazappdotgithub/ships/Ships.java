27f24061a206026b4ca3721971861c13e1e8fc1f
BXG <behxhet.gashi@gmail.com>
2011-06-25 18:16:21 +0200

    Added new Activity + UI. Made the splash screen show for a few sec.

--- src/com/gmail/wazappdotgithub/ships/Ships.java
+++ src/com/gmail/wazappdotgithub/ships/Ships.java
@@ -4,13 +4,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+
+
 public class Ships extends Activity {
+	
+	protected boolean _active = true;
+	protected int _splashTime = 5000;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
-        Intent intent = new Intent(this, PreGame.class);
-        startActivity(intent);
+        
+        /* Will make the splashscreen show for a few sec */
+        
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    
+                    Intent intent = new Intent(Ships.this, UserInput.class);
+                
+                    startActivity(intent);
+                    stop();
+                }
+            }
+        };
+        splashTread.start();
+        
     }
 }
