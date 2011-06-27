c4b6434a07ac65964710702e6498eff8b7b67932
Tor Hammar <mengedej@gmail.com>
2011-06-27 22:49:25 +0200

    new interface for ships

---
+++ src/com/gmail/wazappdotgithub/ships/model/IShip.java
@@ -0,0 +1,13 @@
+/**
+ * @author Tor Hammar, wazapp dot github at gmail dot com
+ */
+
+package com.gmail.wazappdotgithub.ships.model;
+
+
+public interface IShip {
+	int getXposition();
+	int getYPosition();
+	int getSize();
+	boolean isHorizontal();
+}
