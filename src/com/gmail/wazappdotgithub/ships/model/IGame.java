2f1e0458ea2e3110c9739b874e64291b7a048b89
Tor Hammar <mengedej@gmail.com>
2011-09-30 13:45:19 +0200

    Commented the interface

--- src/com/gmail/wazappdotgithub/ships/model/IGame.java
+++ src/com/gmail/wazappdotgithub/ships/model/IGame.java
@@ -6,11 +6,40 @@ import com.gmail.wazappdotgithub.ships.model.Client.IShipsClient;
 
 public interface IGame {
 
+	/**
+	 * Clients use this method to request to start the game.
+	 * 
+	 * @param client the client who called
+	 * @param board the board of the client
+	 */
 	void clientReportReadyForGame(IShipsClient client, IBoard board);
-	Bomb dropBomb(IShipsClient Shootingclient, Bomb b);
 	
+	/**
+	 * Clients use this to evaluate their bombs
+	 * @param client the client who send the bomb
+	 * @param b the bomb to be evaluated
+	 * @return a modified Bomb object with it's flag set to hit or not
+	 */
+	Bomb dropBomb(IShipsClient client, Bomb b);
+	
+	/**
+	 * Clients use this method to request to move to the next state
+	 * @param client the client which request the state change
+	 */
 	void progressState(IShipsClient client);
 	
+	/**
+	 * Call this method to receive the list of all previous Bombs from
+	 * the client which is currently INTURN
+	 * @return a list of historical Bombs 
+	 */
 	List<Bomb> getInTurnClientHistoricalBombs();
+	
+	/**
+	 * Call this method to receive a list of bombs that have been evaluated
+	 * during this turn (may be empty unless the client has evaluated it's 
+	 * bombs)
+	 * @return a list of the latest Bombs
+	 */
 	List<Bomb> getInTurnClientAcceptedBombs();
 }
