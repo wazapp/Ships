0c0bcf62e33eb09b85b0c4c51a7061a87b9d4644
Tor Hammar <mengedej@gmail.com>
2012-01-01 19:26:40 +0100

    Activity Adjustment
    Adjusted the activity life cycle to match the updated Model's way of
    reporting it's state ( RemoteClient & CState & Protocol )
    
    Introduced an ProgressDialog while waiting for opponent

--- src/com/gmail/wazappdotgithub/ships/PreGame.java
+++ src/com/gmail/wazappdotgithub/ships/PreGame.java
@@ -5,11 +5,13 @@ package com.gmail.wazappdotgithub.ships;
 import java.util.Observable;
 import java.util.Observer;
 
-import com.gmail.wazappdotgithub.ships.model.*;
-import com.gmail.wazappdotgithub.ships.model.Client.LocalClient;
+import com.gmail.wazappdotgithub.ships.model.Client.IShipsClient;
+import com.gmail.wazappdotgithub.ships.model.Client.IShipsClient.Statename;
+import com.gmail.wazappdotgithub.ships.model.Client.RemoteClient;
 import com.gmail.wazappdotgithub.ships.model.views.PreGameBoardView;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,11 +21,13 @@ import android.view.View.OnClickListener;
 public final class PreGame extends Activity implements Observer {
 
 	private String tag = "Ships PreGame";
+	private IShipsClient model = RemoteClient.getInstance();
 	
+	ProgressDialog waiting;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		LocalClient.getInstance().addAsObserver(this);
+		model.addAsObserver(this);
 		
 		//Log.d(tag,tag + "configuring pregame contentview");
 		setContentView(R.layout.pregame);
@@ -34,7 +38,7 @@ public final class PreGame extends Activity implements Observer {
 			@Override
 			public void onClick(View v) {
 				Log.d(tag,"randomisingShips");
-				LocalClient.getInstance().getBoard().randomiseShipsLocations();
+				model.getBoard().randomiseShipsLocations();
 				findViewById(R.id.pregame_BoardView).invalidate();
 			}
 		});
@@ -47,7 +51,7 @@ public final class PreGame extends Activity implements Observer {
 				int id = ((PreGameBoardView) findViewById(R.id.pregame_BoardView)).shipIdUnderCursor();
 				Log.d(tag,"rotatingShip id " + id);
 				if ( id >= 0 ) {
-					LocalClient.getInstance().getBoard().toggleOrientation(id);
+					model.getBoard().toggleOrientation(id);
 					findViewById(R.id.pregame_BoardView).invalidate();
 				}
 			}
@@ -58,29 +62,38 @@ public final class PreGame extends Activity implements Observer {
 			
 			@Override
 			public void onClick(View v) {
+				/*
+				 * The player has completed the pregame phase and will 
+				 * now wait for the other player to complete the phase too
+				 * 
+				 *  This will send a message to the other player
+				 */
 				Log.d(tag,"start button pressed");
-				LocalClient.getInstance().reportReady();
+				model.playerCompletedPreGame();
 			}
 		});
 		
-		updateActivity(LocalClient.getInstance().getState());
 	}
 
 	@Override
 	public void update(Observable observable, Object data) {
-		updateActivity((Game.ClientState) data);
+		updateActivity((Statename) data);
 	}	
 	
-	private void updateActivity(Game.ClientState newstate) {
+	private void updateActivity(Statename newstate) {
 		//Log.d(tag,"new state is " + newstate );
 		switch ( newstate ) {
-		case PREGAME : enableInteraction(); break;
+		case PREGAME : disableInteraction(); break;
 		case WAITGAME : progress(); break;
-		default : disableInteraction(); break; 
+		//default : disableInteraction(); break; 
 		}
 	}
+	
 	private void progress() {
-		LocalClient.getInstance().removeAsObserver(this);
+		waiting.setMessage("Connected " + RemoteClient.getInstance().getOpponentName());
+		waiting.dismiss();
+		
+		model.removeAsObserver(this);
 		startActivity(new Intent(this,InGame.class));
 		finish();
 	}
@@ -91,10 +104,13 @@ public final class PreGame extends Activity implements Observer {
 		findViewById(R.id.pregame_button_rotate).setEnabled(true);
 		findViewById(R.id.pregame_button_start).setEnabled(true);
 	}
+	
 	private void disableInteraction() {
 		findViewById(R.id.pregame_BoardView).setEnabled(false);
 		findViewById(R.id.pregame_button_randomize).setEnabled(false);
 		findViewById(R.id.pregame_button_rotate).setEnabled(false);
 		findViewById(R.id.pregame_button_start).setEnabled(false);
+		
+		waiting = ProgressDialog.show(this, "", "Waiting for opponent");
 	}
 }
