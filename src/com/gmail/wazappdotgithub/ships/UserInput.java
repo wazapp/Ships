1e37eec9e334d5c297e474101c51cb53bffa47cb
Tor Hammar <mengedej@gmail.com>
2012-01-01 19:23:46 +0100

    Activity Adjustment
    Adjusted the activity life cycle to match the updated Model's way of
    reporting it's state ( RemoteClient & CState & Protocol )
    
    Introduced an error dialog for Protocol

--- src/com/gmail/wazappdotgithub/ships/UserInput.java
+++ src/com/gmail/wazappdotgithub/ships/UserInput.java
@@ -1,15 +1,26 @@
 package com.gmail.wazappdotgithub.ships;
 
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+import com.gmail.wazappdotgithub.ships.common.Protocol;
+import com.gmail.wazappdotgithub.ships.model.Client.IShipsClient.Statename;
+import com.gmail.wazappdotgithub.ships.common.Protocol.opponentType;
 import com.gmail.wazappdotgithub.ships.model.Client.LocalClient;
-import com.gmail.wazappdotgithub.ships.model.Client.LocalClient.opponentType;
+import com.gmail.wazappdotgithub.ships.model.Client.RemoteClient;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class UserInput extends Activity{
+public class UserInput extends Activity implements Observer{
+	
+	Intent nextActivity;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,27 +28,55 @@ public class UserInput extends Activity{
         
         setContentView(R.layout.userinput);
         
-        
         final Button letsContinueButton = (Button) findViewById(R.id.button1);
         letsContinueButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	
-                // Perform action on click
-            	Intent intent2 = new Intent(UserInput.this, PreGame.class);
-            	LocalClient.newInstance(opponentType.LOCALCOMPUTER); //TODO ensure this clears everything from previous Game
-		        startActivity(intent2);	
+            	try {
+					Protocol.newInstance(opponentType.REMOTECOMPUTER);
+					RemoteClient.newInstance(UserInput.this);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+					AlertDialog.Builder builder = new AlertDialog.Builder(UserInput.this);
+					
+					builder.setMessage("An error occurred establishing the connection")
+					       .setCancelable(false)
+					       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					           public void onClick(DialogInterface dialog, int id) {
+					                dialog.cancel();
+					           }
+					       })
+					       .setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+					           public void onClick(DialogInterface dialog, int id) {
+					        	   UserInput.this.finish();
+					           }
+					       });
+					
+					AlertDialog alert = builder.create();
+					//alert.show() ??
+				}
             }
         });
 
         final Button fleeHome = (Button) findViewById(R.id.button2);
         fleeHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	
                 finish();
                 
             }
         });
              
+	}
+
+	@Override
+	public void update(Observable observable, Object data) {
+		Statename s = ((Statename) data);
+		if (s == Statename.PREGAME) {
+			RemoteClient.getInstance().removeAsObserver(this);
+			startActivity(new Intent(UserInput.this, PreGame.class));
+			finish();
+		}
 	}
 
 }
