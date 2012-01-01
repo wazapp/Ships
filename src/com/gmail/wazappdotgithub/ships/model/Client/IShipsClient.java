9d76010a59498f3fdac0268888419e21d62fe0fd
Tor Hammar <mengedej@gmail.com>
2012-01-01 18:07:07 +0100

    Changed Client Interface Updated / Changed Interface that match the remote client It is very similar to the old interface.
    It is now clearer which methods belong to what area of the program

--- src/com/gmail/wazappdotgithub/ships/model/Client/IShipsClient.java
+++ src/com/gmail/wazappdotgithub/ships/model/Client/IShipsClient.java
@@ -1,63 +1,29 @@
 package com.gmail.wazappdotgithub.ships.model.Client;
 
 import java.util.List;
-import java.util.Observable;
+import java.util.Observer;
 
 import com.gmail.wazappdotgithub.ships.model.Bomb;
-import com.gmail.wazappdotgithub.ships.model.Game;
 import com.gmail.wazappdotgithub.ships.model.IBoard;
-import com.gmail.wazappdotgithub.ships.model.Client.AClient.EndGameData;
+import com.gmail.wazappdotgithub.ships.model.Client.CState.Statename;
 
 public interface IShipsClient {
+	//Observer related
+	public void addAsObserver(Observer obs);
+	public void removeAsObserver(Observer obs);
 
-	// Game uses these to move in states before and after the game is running
-	public void setToStatePreGame();
-	public void setToStateWaitGame();
-	public void setToStatePostGameAsWinner();
-	public void setToStatePostGameAsLooser();
-	
-	/** 
-	 * Call this to request to move away from current state
-	 * The client requests a new state from the Game
-	 */
-	public void requestNextState();
-	
-	/**
-	 * Game to call this to enforce the move to the appropriate state
-	 */
-	public void putToNextState(Game.ClientState newstate);
-	
-	
-	public EndGameData retrieveEndGameData();
-	
-	//possibly sit in another interface!
-	public List<Bomb> getHistoricalBombs();
-	public List<Bomb> getLatestTurnBombs();
-	
-	public List<Bomb> requestInturnClientHistoricalBombs();
-	public List<Bomb> requestInTurnClientAcceptedBombs();
-	
-	public List<Bomb> getInTurnBombs();
-	
-	public Game.ClientState getState();
+	// General access methods 
 	public IBoard getBoard();
+	public Statename getState();
 	
-	public Observable getClientAsObservable();
-	
-	/**
-	 * attempt to place or remove a bomb at the selected coordinates 
-	 * @param xcoord
-	 * @param ycoord
-	 * @return true if the board was updated
-	 */
+	// Protocol related methods
+	public void playerCompletedPreGame();
 	public boolean placeBomb(int xcoord, int ycoord);
-	public void reportReady();
-	
-	/**
-	 * @return the number of live ships on the board
-	 */
-	public int numLiveShips();
 	public int getRemainingBombs();
-
+	public void playerCompletedTurn();
+	public void playerCompletedWait();
+	public List<Bomb> requestInTurnClientAcceptedBombs();
+	public List<Bomb> requestInTurnClientHistoricalBombs();
+	public void playerCompletedEvaluation();
 }
 
