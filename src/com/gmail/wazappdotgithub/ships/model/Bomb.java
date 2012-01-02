f81a649b426ee2cc2b4fde67271e18b19658ede4
Tor Hammar <mengedej@gmail.com>
2012-01-02 15:18:41 +0100

    Bomb now extends the Message class and can read/write itself to a data stream

--- src/com/gmail/wazappdotgithub/ships/model/Bomb.java
+++ src/com/gmail/wazappdotgithub/ships/model/Bomb.java
@@ -1,11 +1,17 @@
 package com.gmail.wazappdotgithub.ships.model;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import com.gmail.wazappdotgithub.ships.common.Message;
 
 /*
  * Used to transfer information
  */
-public final class Bomb {
+public final class Bomb extends Message {
 
+	private static final MessageType type = MessageType.BOMB_MESSAGE;
 	public int x;
 	public int y;
 	public boolean hit;
@@ -33,5 +39,29 @@ public final class Bomb {
 		return false;	
 	}
 	
+	@Override
+	public MessageType getType() {
+		return type;
+	}
 	
+	@Override
+	public void writeTo(DataOutputStream out) throws IOException {		
+		out.writeInt(type.ordinal());
+		out.writeInt(x);
+		out.writeInt(y);
+		out.writeBoolean(hit);
+		out.writeBoolean(destrship);
+	}
+
+	@Override
+	public void readFrom(DataInputStream in) throws IOException {
+		int ordinal = in.readInt();
+		if ( ordinal != type.ordinal() )
+			throw new RuntimeException("Error in protocol, did not read expected ordinal");
+		
+		x = in.readInt();
+		y = in.readInt();
+		hit = in.readBoolean();
+		destrship = in.readBoolean();
+	}
 }
