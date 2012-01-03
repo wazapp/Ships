e6221a0165b2c35b5ca1db381bf288d132378cff
Tor Hammar <mengedej@gmail.com>
2012-01-03 14:19:32 +0100

    Adjustments to protocol and asynchronos tasks, minor error fixes

--- src/com/gmail/wazappdotgithub/ships/model/Client/CState.java
+++ src/com/gmail/wazappdotgithub/ships/model/Client/CState.java
@@ -1,24 +1,40 @@
 package com.gmail.wazappdotgithub.ships.model.Client;
 
-import java.util.LinkedList;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.gmail.wazappdotgithub.ships.common.EndMessage;
 import com.gmail.wazappdotgithub.ships.common.StartBombMessage;
 import com.gmail.wazappdotgithub.ships.common.Protocol;
 import com.gmail.wazappdotgithub.ships.common.ReadyMessage;
-import com.gmail.wazappdotgithub.ships.model.BoardUsingSimpleShip;
 import com.gmail.wazappdotgithub.ships.model.Bomb;
 import com.gmail.wazappdotgithub.ships.model.Client.IShipsClient.Statename;
 import com.gmail.wazappdotgithub.ships.model.Client.RemoteClient;
 
-public abstract class CState {
+/*
+ * RULES and DESCRIPTION
+ * enterTurnEval() and enterWaitEval() spawns an Aynchronous Task
+ * to deal with the network communication.
+ * These threads will notify Observers (UI) when completed.
+ * 
+ * INIT states does not notify at all
+ * 
+ * an EXITstate will notify the UI immediately and automatically
+ * move to the next state
+ * 
+ * an ENTERstate will notify when completed and stay there
+ * 
+ * The Client (UI) can only request EXITstate methods.
+ */
+
+public final class CState {
 	private static RemoteClient cl;
-	
-	public static void setClient(RemoteClient c) {
+	private static String tag = "Ships CState ";
+
+	public static void initClient(RemoteClient c) {
 		cl = c;
-		enterState(Statename.PREGAME);
 	}
-	
+
 	/*
 	 * The purpose of running an enterState() method is to 
 	 * prepare the client for the coming UI reading methods.
@@ -28,18 +44,19 @@ public abstract class CState {
 	 */
 	private static void enterState(Statename s) {
 		switch (s) {
-		case PREGAME : enterPreGame();
-		case WAITGAME : enterWaitGame();
-		case TURN : enterTurn();
-		case WAIT : enterWait();
-		case TURN_EVAL : enterTurnEval();
-		case WAIT_EVAL : enterWaitEval();
-		case GAMEOVER : enterGameOver();
+		//case INIT : enterInit();
+		case PREGAME : enterPreGame(); break;
+		case WAITGAME : enterWaitGame(); break;
+		case TURN : enterTurn(); break;
+		case WAIT : enterWait(); break;
+		case TURN_EVAL : enterTurnEval(); break;
+		case WAIT_EVAL : enterWaitEval(); break;
+		case GAMEOVER : enterGameOver(); break;
 		default : throw new IllegalStateException("No such state " + s);
 		}
-		
+
 	}
-	
+
 	/*
 	 * The purpose of running an exitState() method is to push the model from
 	 * the old state to the new state. This should NOT affect the UI. 
@@ -49,84 +66,85 @@ public abstract class CState {
 	 */
 	protected static void exitState(Statename s) {
 		switch (s) {
-		case PREGAME : exitPreGame();
-		case WAITGAME : exitWaitGame();
-		case TURN : exitTurn();
-		case WAIT : exitWait();
-		case TURN_EVAL : exitTurnEval();
-		case WAIT_EVAL : exitWaitEval();
+		case INIT : exitInit();  break;
+		case PREGAME : exitPreGame(); break;
+		case WAITGAME : exitWaitGame(); break;
+		case TURN : exitTurn(); break;
+		case WAIT : exitWait(); break;
+		case TURN_EVAL : exitTurnEval(); break;
+		case WAIT_EVAL : exitWaitEval(); break;
 		default : throw new IllegalStateException("No such state " + s);
 		}
-		
+
 	}
-	
+
 	/* Convenience, used throughout the class */ 
 	private static void stateUpdate(Statename newstate) {
+		Log.d(tag,tag+"changing state to " + newstate);
 		cl.state = newstate;
 		cl.setToChanged();
 		cl.notifyObservers(newstate);
 	}
-	
+
+	/*
+	 * NOTE the INIT states does not notify the UI
+	 */
+	private static void exitInit() {
+		enterState(Statename.PREGAME);
+		//nothing to be done here
+	}
+
 	/*
 	 * Called to prepare the client model, before starting any game
 	 */
 	private static void enterPreGame() {
-		//prepare local data
-		cl.board = new BoardUsingSimpleShip();
-		cl.historicalBombs = new LinkedList<Bomb>();
-		cl.latestTurnBombs = new LinkedList<Bomb>();
-		cl.inturnBombs = new LinkedList<Bomb>();
-		
-		//preparing remote data
-		cl.r_historicalBombs = new LinkedList<Bomb>();
-		cl.r_inturnBombs = new LinkedList<Bomb>();
-		
-		//let UI know it can modify the board etc.
+		//nothing to be done here
 		stateUpdate(Statename.PREGAME);
 	};
-	
+
 	/*
 	 * The User has chosen their configuration and is 
 	 * prepared to progress
 	 */
 	private static void exitPreGame() {
 		stateUpdate(Statename.PREGAME_EXIT);
-		
+
 		try {
 			cl.board.finalise();
 			Protocol.getInstance().send(new ReadyMessage("Player", cl.starter));
-			
+
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Main thread waited for blocking queue");
 		}
-		
-		enterState(Statename.WAIT);
+
+		enterState(Statename.WAITGAME);
 	};
 	/*
 	 * Wait until the ready message arrives from the opponent
 	 * When it does, progress
 	 */
 	private static void enterWaitGame() {
+		//nothing to be done here
 		stateUpdate(Statename.WAITGAME);
 	};
-	
+
 	/*
 	 * Go to either Turn or Wait state
 	 */
 	private static void exitWaitGame() {
+		stateUpdate(Statename.WAITGAME_EXIT);
+		
 		try {
 			ReadyMessage ready;
 			ready = (ReadyMessage) Protocol.getInstance().retrieve();
 			//read some data from the Message
 			cl.opponentName = ready.nickname;
-			
+
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		stateUpdate(Statename.WAITGAME_EXIT);
 		
 		// Start whoever was designated as starter
 		if ( cl.starter )
@@ -134,20 +152,19 @@ public abstract class CState {
 		else 
 			enterState(Statename.WAIT);
 	};
-	
+
 	/*
 	 * Let the client enter TURN state where the user can place bombs to be sent
 	 */
 	private static void enterTurn() {
-		//Manage bombs ( both remote and local )
 		cl.manageBombsForStateSwitch();
-		
+
 		// use rules to determine the number of bombs to restock.
 		cl.bombstoplace = cl.getBoard().numLiveShips();
-		
+
 		stateUpdate(Statename.TURN);
 	};
-	
+
 	/*
 	 * The User have accepted all the bombs and has asked to get hem evaluated
 	 * and to view the result
@@ -156,46 +173,58 @@ public abstract class CState {
 		stateUpdate(Statename.TURN_EXIT);
 		enterState(Statename.TURN_EVAL);
 	};
-	
+
 	/*
 	 * Send / Recieve the bombs, also evaluate the game state (game over or not)
 	 */
 	private static void enterTurnEval() {
-		try {
-			//Let the protocol know how many bombs to expect
-			StartBombMessage sbm = new StartBombMessage();
-			sbm.number = cl.inturnBombs.size();
-			Protocol.getInstance().send(sbm);
-			
-			//TODO, compress this s little you can send / retreive in a single loop
-			//send them
-			for (Bomb b : cl.inturnBombs) {
-				Protocol.getInstance().send(b);
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				Thread.currentThread().setName("Turn Evaluation networking");
+				try {
+					Protocol network = Protocol.getInstance();
+					
+					//Let the protocol know how many bombs to expect
+					StartBombMessage sbm = new StartBombMessage();
+					sbm.number = cl.inturnBombs.size();
+					network.send(sbm);
+
+					//send them
+					for (Bomb b : cl.inturnBombs) {
+						network.send(b);
+					}
+
+					// replace the sent bombs with the received 
+					// bombs that contain hit information
+					cl.inturnBombs.clear();
+					
+					StartBombMessage s = (StartBombMessage) network.retrieve();
+					int count = s.number;
+					
+					for (int i = 0; i < count; i++) {
+						Bomb b = (Bomb) network.retrieve();
+						cl.inturnBombs.add(b);
+					}
+
+					//read if the game is over
+					EndMessage end = (EndMessage) network.retrieve();
+					cl.is_game_over = end.isGameOver;
+
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				return null;
+			}
+			protected void onPostExecute(Void result) {
+				stateUpdate(Statename.TURN_EVAL);
 			}
 			
-			// replace the sent bombs with the received 
-			// bombs that contain hit information
-			StartBombMessage s = (StartBombMessage) Protocol.getInstance().retrieve();
-			int count = s.number;
-			cl.inturnBombs.clear();
-			
-			for (int i = 0; i < count; i++) {
-				Bomb b = (Bomb) Protocol.getInstance().retrieve();
-				cl.inturnBombs.add(b);
-			}
-			
-			//read if the game is over
-			EndMessage end = (EndMessage) Protocol.getInstance().retrieve();
-			cl.is_game_over = end.isGameOver;
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		stateUpdate(Statename.TURN_EVAL);
+		}.execute();
 	};
-	
+
 	/*
 	 * Bombs have been evaluated
 	 */
@@ -203,20 +232,18 @@ public abstract class CState {
 		stateUpdate(Statename.TURN_EVAL_EXIT);
 		if ( cl.is_game_over )
 			enterState(Statename.GAMEOVER);
-		
-		enterState(Statename.WAIT);
+		else
+			enterState(Statename.WAIT);
 	};
-	
+
 	/*
 	 * method is called when the player has exited the TURNeval state
 	 */
 	private static void enterWait() {
-		// manage local bombs
 		cl.manageBombsForStateSwitch();
-		
 		stateUpdate(Statename.WAIT);
 	};	
-	
+
 	/*
 	 * Exit the wait state
 	 */
@@ -224,44 +251,68 @@ public abstract class CState {
 		stateUpdate(Statename.WAIT_EXIT);
 		enterState(Statename.WAIT_EVAL);
 	};
-	
+
 	/*
 	 * Enter wait evaluation, where we attempt to retrieve the remote bombs
 	 * and evaluate them
 	 */
 	private static void enterWaitEval() {
-		// read in remote data
-		try {
-			//find out how many bombs the opponent sends
-			StartBombMessage s = (StartBombMessage) Protocol.getInstance().retrieve();
-			int count = s.number;
-			
-			//expect to send the same number back
-			Protocol.getInstance().send(s);
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				try {
+					Thread.currentThread().setName("Wait Evaluation networking");
+					Protocol network = Protocol.getInstance();
+					
+					//find out how many bombs the opponent sends
+					StartBombMessage s = (StartBombMessage) network.retrieve();
+					int count = s.number;
 
-			//retrieve the remote bombs
-			for (int i = 0; i < count; i++) {
-				Bomb b = (Bomb) Protocol.getInstance().retrieve();
+					if ( count == 0 )
+						throw new RuntimeException(tag + "Scheduled to recieve 0 bombs");
+					Log.d(tag, tag+"recieving and evaluating"+count+" bombs");
+										
+					//retrieve the remote bombs
+					for (int i = 0; i < count; i++) {
+						Bomb b = (Bomb) network.retrieve();
+						// Bomb the coordinate locally and place the bomb in
+						// the local cache
+						Log.d(tag, tag+"evaluating"+i);
+						b = cl.getBoard().bombCoordinate(b);
+						cl.r_inturnBombs.add(b);
+						
+					}
+					
+					if ( cl.r_inturnBombs.size() != count )
+						throw new RuntimeException(tag + "local cache mismatch, possibly enter TURN before completion");
+					
+					//expect to send the same number back
+					network.send(s);
+					
+					//send response back
+					int x = 0;
+					for ( Bomb b : cl.r_inturnBombs) {
+						network.send(b);
+						x++;
+					}
+					
+					Log.d(tag, tag+"schedule for send "+ x + " bombs");
+					//send the state of the game to the opponent
+					cl.is_game_over = cl.getBoard().numLiveShips() == 0;
+					network.send(new EndMessage(cl.is_game_over));
 
-				// Bomb the coordinate locally and place the bomb in
-				// the local cache
-				b = cl.getBoard().bombCoordinate(b);
-				cl.r_inturnBombs.add(b);
-				
-				//send response back
-				Protocol.getInstance().send(b);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				return null;
 			}
-			
-			//send the state of the game to the opponent
-			cl.is_game_over = cl.getBoard().numLiveShips() == 0;
-			Protocol.getInstance().send(new EndMessage(cl.is_game_over));
 
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			protected void onPostExecute(Void result) {
+				stateUpdate(Statename.WAIT_EVAL);
+			}	
 
-		stateUpdate(Statename.WAIT_EVAL);
+		}.execute();
 	};
 
 	/*
@@ -272,10 +323,10 @@ public abstract class CState {
 		stateUpdate(Statename.WAIT_EVAL_EXIT);
 		if ( cl.is_game_over )
 			enterState(Statename.GAMEOVER);
-		
-		enterState(Statename.TURN);
+		else
+			enterState(Statename.TURN);
 	};
-	
+
 	/*
 	 * Evaluate if the client is the winner or not and let the UI know
 	 */
